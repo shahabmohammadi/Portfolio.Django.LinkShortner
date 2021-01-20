7e63a466bc636863d1d11c1477fc3e04985049f3
Shahab Mohammadi <develop.shahab@Gmail.com>
2021-01-21 00:24:58 +0330

    app added to project

--- URL_Shortener_Django/settings.py
+++ URL_Shortener_Django/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortenerApp.apps.ShortenerappConfig',
 ]
 
 MIDDLEWARE = [
