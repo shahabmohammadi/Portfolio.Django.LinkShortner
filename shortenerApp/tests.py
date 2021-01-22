ccf375a4f694c7ff1c1e24e3b46752a495178ef2
Shahab Mohammadi <develop.shahab@Gmail.com>
2021-01-22 14:41:24 +0330

    some tests added to project

--- shortenerApp/tests.py
+++ shortenerApp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from shortenerApp.models import Urls
+
+urls = {
+    "Digikala": "https://www.digikala.com/",
+    "DigiPay": "https://www.mydigipay.com/"
+}
+
+
+class AnimalTestCase(Urls):
+    def setUp(self):
+        Urls.objects.create(url=urls["Digikala"])
+        Urls.objects.create(url=urls["DigiPay"])
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Urls.objects.get(url=urls["Digikala"])
+        cat = Urls.objects.get(url=urls["DigiPay"])
+        self.assertEqual(lion.speak(), urls["Digikala"])
+        self.assertEqual(cat.speak(), urls["DigiPay"])
