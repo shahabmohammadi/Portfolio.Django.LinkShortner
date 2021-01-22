2d088ba043a3cec521e3b4d1cab3941cc2a33bfe
Shahab Mohammadi <develop.shahab@Gmail.com>
2021-01-22 14:05:46 +0330

    requirements added to settings of project

--- URL_Shortener_Django/settings.py
+++ URL_Shortener_Django/settings.py
@@ -35,8 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'qr_code',
     'shortenerApp.apps.ShortenerappConfig',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
